51d490e18843ee11954d20439e9b836fc08aac3b
sbx_user1051 <sbx_user1051@169.254.29.189>
2025-12-06 17:14:16 +0000

    Sat Dec 06 17:14:16 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,19 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        if not isinstance(skus, str):
+            return -1 
+
+        valid_items = {'A','B','C','D'}
+
+        for char in skus:
+            if char not in valid_items:
+                return -1
+
+        a = skus.count('A')
+        b = skus.count('B')
+        c = skus.count('C')
+        d = skus.count('D')
+        
+        return (a//3)*130 + (a%3)*50 + (b//2)*45 + (b%2)*30 + c*20 + d*15
+
