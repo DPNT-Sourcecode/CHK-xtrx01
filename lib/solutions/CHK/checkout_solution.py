16f36c83e7ad4057fe92524a2a02b5a840261244
sbx_user1051 <sbx_user1051@169.254.29.189>
2025-12-06 19:02:47 +0000

    Sat Dec 06 19:02:47 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -21,24 +21,43 @@ class CheckoutSolution:
         total = 0 
 
         total += (counts['A'] // 5) * 200 + ((counts['A'] % 5) // 3) * 130 + ((counts['A'] % 5) % 3) * 50 
-
         total += (counts['B'] // 2) * 45 + (counts['B'] % 2) * 30  
         total += (counts['F'] - counts['F'] // 3) * 10
         total += (counts['H'] // 10) * 80 + ((counts['H'] % 10) //5) * 45 + (counts['H'] % 5) * 10   
-        total += (counts['K'] // 2) * 150 + (counts['K'] % 2) * 80  
+        total += (counts['K'] // 2) * 120 + (counts['K'] % 2) * 70  
         total += (counts['P'] // 5) * 200 + (counts['P'] % 5) * 50  
         total += (counts['Q'] // 3) * 80 + (counts['Q'] % 3) * 30  
         total += (counts['U'] - counts['U'] // 4) * 40  
         total += (counts['V'] // 3) * 130 + ((counts['V'] % 3) // 2) * 90  + ((counts['V'] % 3) % 2) * 50
 
-        prices = {
+        group_items = ['S','T','X','Y','Z']
+
+        group_counts = {item: counts[item] for item in group_items}
+
+        group_total_items = sum(group_counts.values())
+
+        group_offers = group_total_items // 3
+
+        group_remainder = group_total_items % 3
+
+        total += group_offers * 45
+
+        items_by_price = [('Z', 21), ('S', 20), ('T', 20), ('Y', 20), ('X', 17)]
+
+        for item, price in reversed(items_by_price):
+            while group_counts[item] > 0 and group_remainder > 0:
+                total += price
+                group_counts[item] -= 1
+                group_remainder -= 1
+
+        simple_prices = {
             'C':20, 'D':15, 'E':40, 'G':20, 'I':35, 'J':60, 'L':90,
-            'M':15, 'N':40, 'O':10, 'R':50, 'S':30, 'T':20, 'W':20,
-            'X':90, 'Y':10, 'Z':50
+            'M':15, 'N':40, 'O':10, 'R':50, 'W':20
         }
        
-        for item, price in prices.items():
+        for item, price in simple_prices.items():
             total += counts[item] * price
 
         return total
+
 
