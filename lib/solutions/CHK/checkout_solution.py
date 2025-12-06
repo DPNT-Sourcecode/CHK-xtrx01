293bb60d7d22c307aa754f61d4b30ef55b82294e
sbx_user1051 <sbx_user1051@169.254.29.189>
2025-12-06 17:41:29 +0000

    Sat Dec 06 17:41:29 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,8 +6,6 @@ class CheckoutSolution:
         if not isinstance(skus, str):
             return -1 
 
-        skus = skus.upper()
-        
         valid_items = {'A','B','C','D','E'}
 
         for char in skus:
@@ -42,4 +40,5 @@ class CheckoutSolution:
         e_total = e * 40
 
         return a_total + b_total + c_total + d_total + e_total
+
 
