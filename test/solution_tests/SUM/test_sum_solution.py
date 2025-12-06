7530837d18b0f01330a28b42adca4d35d9ad1f8d
sbx_user1051 <sbx_user1051@169.254.29.189>
2025-12-06 16:35:38 +0000

    Sat Dec 06 16:35:38 UTC 2025

--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -1,6 +1,10 @@
-from solutions.SUM.sum_solution import SumSolution
 
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', '..', 'lib'))
+from solutions.SUM.sum_solution import SumSolution
 
 class TestSum():
     def test_sum(self):
         assert SumSolution().compute(1, 2) == 3
+
