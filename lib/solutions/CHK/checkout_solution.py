308175362c4413c8b1a60cc8b1f23066bac1122c
sbx_user1051 <sbx_user1051@169.254.29.189>
2025-12-06 17:50:39 +0000

    Sat Dec 06 17:50:39 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,7 +6,7 @@ class CheckoutSolution:
         if not isinstance(skus, str):
             return -1 
 
-        valid_items = {'A','B','C','D','E'}
+        valid_items = {'A','B','C','D','E','F'}
 
         for char in skus:
             if char not in valid_items:
@@ -17,28 +17,21 @@ class CheckoutSolution:
         c = skus.count('C')
         d = skus.count('D')
         e = skus.count('E')
+        f = skus.count('F')
 
         free_b_e = e // 2
         b = max(0, b - free_b_e)
 
-        a_total = 0
+        f_to_pay = f - (f // 3)
 
-        five_a = a // 5
-        a_total += five_a * 200
-        a_remaining = a % 5
-
-        three_a = a_remaining // 3
-        a_total += three_a * 130
-        a_remaining = a_remaining % 3
-
-        a_total += a_remaining * 50
-
+        a_total = (a // 5) * 200 + ((a % 5) // 3) * 130 + ((a % 5) // 3) * 50
         b_total = (b // 2) * 45 + (b % 2) * 30 
-
         c_total = c * 20
         d_total = d * 15
         e_total = e * 40
+        f_total = f_to_pay * 10
 
-        return a_total + b_total + c_total + d_total + e_total
+        return a_total + b_total + c_total + d_total + e_total + f_total
+
 
 
