1a03dbf01feea31c0c20b2924e12e08d4ff5e0ad
sbx_user1051 <sbx_user1051@169.254.29.189>
2025-12-06 18:23:25 +0000

    Sat Dec 06 18:23:25 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,18 +6,20 @@ class CheckoutSolution:
         if not isinstance(skus, str):
             return -1 
 
-        valid_items = {'A','B','C','D','E','F'}
+        valid_items = set('ABCDEFGHIJKLMNOPQKRSTUVWXYZ')
 
         for char in skus:
             if char not in valid_items:
                 return -1
 
-        a = skus.count('A')
-        b = skus.count('B')
-        c = skus.count('C')
-        d = skus.count('D')
-        e = skus.count('E')
-        f = skus.count('F')
+        counts = {item: skus.count(item) for item in valid_items}
+
+        counts['B'] = max(0, counts['B'] - counts['E'] // 2 )
+        counts['M'] = max(0, counts['M'] - counts['N'] // 2 )
+        counts['Q'] = max(0, counts['Q'] - counts['R'] // 2 )
+
+        total = 0 
+        total += (counts['A'] // 5) * 200 + ((counts['A'] % 5) // 3) * 130 + ((counts['A'] % 5) % 3) * 50 
 
         free_b_e = e // 2
         b = max(0, b - free_b_e)
@@ -32,4 +34,5 @@ class CheckoutSolution:
         f_total = f_to_pay * 10
 
         return a_total + b_total + c_total + d_total + e_total + f_total
+
 
