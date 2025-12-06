9cbc3f7e2794f7368f9e3bcda6bd59f725e517b8
sbx_user1051 <sbx_user1051@169.254.29.189>
2025-12-06 17:29:25 +0000

    Sat Dec 06 17:29:25 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,7 +6,7 @@ class CheckoutSolution:
         if not isinstance(skus, str):
             return -1 
 
-        valid_items = {'A','B','C','D'}
+        valid_items = {'A','B','C','D','E'}
 
         for char in skus:
             if char not in valid_items:
@@ -16,5 +16,28 @@ class CheckoutSolution:
         b = skus.count('B')
         c = skus.count('C')
         d = skus.count('D')
-        
-        return (a//3)*130 + (a%3)*50 + (b//2)*45 + (b%2)*30 + c*20 + d*15
+        e = skus.count('E')
+
+        free_b_e = e // 2
+        b = max(0, b - free_b_e)
+
+
+        five_a = a // 5
+        a_total += five_a * 200
+        a_remaining = a % 5
+
+        three_a = a_remaining // 3
+        a_total += three_a * 130
+        a_remaining = a_remaining % 3
+
+        a_total += a_remaining * 50
+
+        b_total = (b // 2) * 45 + (b % 2) * 30 
+
+        c_total = c * 20
+        d_total = d * 15
+        e_total = e * 40
+
+        return a_total + b_total + c_total + d_total + e_total
+
+
